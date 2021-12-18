20fb29fdfe114b6c2a04bd2f9b2693a4bddaea04
Augusto Moita <augustomoita.94@gmail.com>
2021-12-18 12:27:43 -0300

    Retorna Promise por ser async

--- src/api.ts
+++ src/api.ts
@@ -2,21 +2,19 @@ const api = {
   gifts: {
     list: async function (): Promise<Gift[]> {
       const storedGifts = localStorage.getItem('gifts') || '[]';
-      return new Promise((resolve) => {
-        setTimeout(() => {
-          resolve(JSON.parse(storedGifts));
-        }, 1000);
-      });
+      await sleep(1000);
+      return JSON.parse(storedGifts) as Gift[];
     },
     save: async function (gifts: Gift[]): Promise<boolean> {
-      return new Promise((resolve) => {
-        localStorage.setItem('gifts', JSON.stringify(gifts));
-        setTimeout(() => {
-          resolve(true);
-        }, 100);
-      });
+      localStorage.setItem('gifts', JSON.stringify(gifts));
+      await sleep(100);
+      return true;
     },
   },
 };
+
+function sleep(ms: number): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
 export default api;
