29b4266cebb2dbfc5ab4e3a6e64dab413aacf36d
Augusto Moita <augustomoita.94@gmail.com>
2021-12-20 22:13:25 -0300

    Oculta el modal si hace click afuera

--- src/components/GiftItemSimple.tsx
+++ src/components/GiftItemSimple.tsx
@@ -7,15 +7,17 @@ type Props = {
 function GiftItemSimple({ gift }: Props) {
   const { name, image, receiver, qty } = gift;
   return (
-    <span className="flex items-center">
-      <img src={image} alt={name} className="h-20 w-20 p-1" />
-      <div className="text-left">
-        <p className="uppercase text-lg font-medium mx-2">
-          {name} ({qty})
-        </p>
-        <p className="capitalize text-sm text-gray-400 mx-2">{receiver}</p>
-      </div>
-    </span>
+    <li className="flex justify-between items-center my-2 py-2 px-2 border-2 shadow-sm hover:shadow-md">
+      <span className="flex items-center">
+        <img src={image} alt={name} className="h-20 w-20 p-1" />
+        <div className="text-left">
+          <p className="uppercase text-lg font-medium mx-2">
+            {name} ({qty})
+          </p>
+          <p className="capitalize text-sm text-gray-400 mx-2">{receiver}</p>
+        </div>
+      </span>
+    </li>
   );
 }
 
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -7,6 +7,7 @@ type Props = {
 
 const Modal: React.FC<Props> = ({ children, visible = true, onClose }) => {
   const visibility = visible ? 'visible' : 'invisible';
+
   useEffect(() => {
     const escFunction = (event: KeyboardEvent) => {
       if (event.key === 'Escape') {
@@ -15,19 +16,37 @@ const Modal: React.FC<Props> = ({ children, visible = true, onClose }) => {
         }
       }
     };
-    document.addEventListener('keydown', escFunction, false);
+
+    const clickOutside = (event: MouseEvent) => {
+      if (onClose) {
+        onClose();
+      }
+    };
+
+    if (visible) {
+      document.addEventListener('keydown', escFunction, false);
+      document.addEventListener('click', clickOutside, false);
+    }
 
     return () => {
       document.removeEventListener('keydown', escFunction, false);
+      document.removeEventListener('click', clickOutside, false);
     };
-  }, [onClose]);
+  }, [onClose, visible]);
+
+  const stopPropagation = (event: React.MouseEvent) => {
+    event.stopPropagation();
+  };
+
   return (
     <div
       className={`${visibility} min-w-screen h-screen animated fadeIn faster fixed left-0 top-0 flex justify-center items-center inset-0 z-50 outline-none focus:outline-none bg-no-repeat bg-center bg-cover`}
-      id="modal-id"
     >
       <div className="absolute bg-black opacity-80 inset-0 z-0"></div>
-      <div className="w-full  max-w-lg p-5 relative mx-auto my-auto rounded-xl shadow-lg bg-white ">
+      <div
+        className="w-full max-w-lg p-5 relative mx-auto my-auto rounded-xl shadow-lg bg-white"
+        onClick={stopPropagation}
+      >
         <div className="">{children}</div>
       </div>
     </div>
